a23f94c128fbd681a8de4fdc5fce0399d41b8467
Surajit <surajitjana20107@gmail.com>
2026-05-30 19:39:57 +0530

    feat: implemented add product to cart feature

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
+import { Toaster } from "sonner-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} initialRouteName="(main)" />
-  )
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} initialRouteName="(main)" />
+      <Toaster position="bottom-center" />
+    </GestureHandlerRootView>
+  );
 }
